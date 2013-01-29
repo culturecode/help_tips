a0bbc2e4d78142e90ba59c1f57724135a83a0196
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2013-01-29 00:21:37 -0800

    Made help modals work.

--- app/helpers/help_tips_helper.rb
+++ app/helpers/help_tips_helper.rb
@@ -13,12 +13,15 @@ module HelpTipsHelper
     render('help_tips/help_tip', :title => title, :body => body, :identifier => identifier) unless TipHide.hidden?(current_user, identifier)
   end
 
-  def help_modal(title = nil, &block)
-    content_for(:help_modal) { render('help_tips/help_modal', :title => title, :body => capture(&block)) }
+  def help_modal_content(title = nil, body= nil, &block)
+    if block_given?
+      body = capture(&block)
+    end
+    content_for(:help_modal) { render('help_tips/help_modal', :title => title, :body => body) }
   end
 
-  def help_button
-    icon = '<i class="icon-question-sign"></i> '.html_safe
-    link_to icon + 'Help', "#help-modal", :class => 'btn', :data => {:toggle => 'modal'}
+  def help_button(html_options = {})
+    html_options.reverse_merge! :class => 'btn', 'data-toggle' => 'modal'
+    link_to('<i class="icon-question-sign"></i> Help'.html_safe, "#help-modal", html_options)
   end  
 end
