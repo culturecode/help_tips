bc45766570d811afbbedaecd0c0b39c28519b746
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2013-02-17 11:05:58 -0800

    Made if possible to specify a footer.

--- app/helpers/help_tips_helper.rb
+++ app/helpers/help_tips_helper.rb
@@ -13,15 +13,20 @@ module HelpTipsHelper
     render('help_tips/help_tip', :title => title, :body => body, :identifier => identifier) unless TipHide.hidden?(current_user, identifier)
   end
 
-  def help_modal_content(title = nil, body= nil, &block)
+  def help_modal_content(title = nil, body = nil, footer = nil, &block)
     if block_given?
+      footer = body
       body = capture(&block)
     end
-    content_for(:help_modal) { render('help_tips/help_modal', :title => title, :body => body) }
+    content_for(:help_modal) { render('help_tips/help_modal', :title => title, :body => body, :footer => footer) }
   end
 
   def help_button(html_options = {})
     html_options.reverse_merge! :class => 'btn', 'data-toggle' => 'modal'
     link_to('<i class="icon-question-sign"></i> Help'.html_safe, "#help-modal", html_options)
-  end  
+  end
+
+  def help_modal_cancel_button
+    link_to('Cancel', '#', :class => 'btn', 'data-dismiss' => 'modal')
+  end
 end
