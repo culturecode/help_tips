1513040199befe7dfec094542f768731415128bb
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2014-02-28 17:31:37 -0400

    Rails 4 compatibility.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match 'help_tips/hide' => 'help_tips#hide', :as => :hide_help_tips
-  match 'help_tips/reset' => 'help_tips#reset', :as => :reset_help_tips
+  match 'help_tips/hide' => 'help_tips#hide', :as => :hide_help_tips, :via => [:get, :post]
+  match 'help_tips/reset' => 'help_tips#reset', :as => :reset_help_tips, :via => [:get, :post]
 end
